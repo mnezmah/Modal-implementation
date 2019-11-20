0e166fcd794a2a31a760805d4d41f51bd4bcdabe
mnezmah <mario.nezmah@gmail.com>
2019-11-20 14:12:48 +0100

    Move JavaScript to config.js

--- source/javascript/config/index.js
+++ source/javascript/config/index.js
@@ -15,3 +15,116 @@ if (!Environment.isLocal) {
   removeServiceWorker()
 }
 // ----
+
+import API from '@utilities/api'
+
+console.log('Hi from config-JS  😃')
+
+const modal = document.querySelector('#modal-subsc')
+const openButton = document.querySelector('.hero__cta')
+const videoDiv = document.querySelector('.video__player')
+const closeButton = document.querySelector('.modal__button-close')
+const emailInput = document.querySelector('#email')
+const firstNameInput = document.querySelector('.input__first')
+const lastNameInput = document.querySelector('.input__last')
+const firstInput = document.querySelector('#first')
+const lastInput = document.querySelector('#last')
+
+//open modal with button 'open modal'
+openButton.addEventListener('click', openModal)
+function openModal() {
+  console.log('I am clicked')
+  modal.classList.toggle('modal--is-showing')
+  modal.classList.add('modal-subsc')
+
+  //pause the video
+  videoDiv.firstChild.pause()
+
+  //show the close button
+  closeButton.style.visibility = 'visible'
+
+  //style the close button
+  closeButton.classList.add('modal__button-round')
+
+  //hide the subscribe button
+  openButton.classList.add('hidden')
+}
+
+//close modal with x button
+closeButton.addEventListener('click', closeModal)
+closeButton.addEventListener('keypress', closeModal)
+function closeModal() {
+  console.log('Closing the modal')
+  modal.classList.toggle('modal--is-showing')
+
+  //resume the video
+  videoDiv.firstChild.play()
+
+  //show the subscribe button
+  openButton.classList.remove('hidden')
+}
+
+emailInput.addEventListener('click', showHiddenForm)
+function showHiddenForm() {
+  console.log('email clicked!')
+  firstNameInput.classList.remove('hidden')
+  lastNameInput.classList.remove('hidden')
+  document.querySelector('.modal__button-back').classList.remove('hidden')
+  document.querySelector('.modal__button-submit').classList.remove('hidden')
+}
+
+//Form validation
+const submitButton = document.querySelector('.modal__button-submit')
+
+submitButton.addEventListener('click', validate)
+function validate() {
+  if (emailInput.value == '') {
+    alert('Please provide your Email!')
+    emailInput.focus()
+    return false
+  }
+  if (firstInput.value == '') {
+    alert('Please provide your First name!')
+    firstInput.focus()
+    return false
+  }
+  if (lastInput.value == '') {
+    alert('Please provide your Last name!')
+    lastInput.focus()
+    return false
+  }
+  validateEmail()
+  // API call
+  callApi()
+  return true
+}
+
+function callApi() {
+  API.post('https://jsonplaceholder.typicode.com/usersd', {
+    data: {
+      name: `${firstInput.value} ${lastInput.value}`,
+      email: `${emailInput.value}`,
+    },
+  })
+    .then(response => console.log('response :', response))
+    .catch(response => console.log('error:', response))
+}
+
+function validateEmail() {
+  const emailValue = emailInput.value
+  const atPosition = emailValue.indexOf('@')
+  const dotPosition = emailValue.lastIndexOf('.')
+  if (atPosition < 1 || dotPosition - atPosition < 2 || dotPosition == emailValue.length - 1) {
+    alert('Please enter a valid e-mail address')
+    emailInput.focus()
+    return false
+  }
+
+  //hide the modal
+  modal.classList.toggle('hidden')
+  //show success toast
+  document.querySelector('.toast__bedankt').classList.remove('hidden')
+  //resume the video
+  videoDiv.firstChild.play()
+  return true
+}
